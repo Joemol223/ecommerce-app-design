402a774ba9ca4b6425f6118a4630ffaa6e2a3757
Joemol Joy <joemoljoy.a@gmail.com>
2024-04-18 13:32:35 -0400

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Joemol_Joy_Project1"
+include(":app")
+ 
